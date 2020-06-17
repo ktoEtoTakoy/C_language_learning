b42bca18d14b4ac3d7cd100da0bd810bf540607a
AndreiShchepa <andrewbandit228@gmail.com>
2020-06-17 23:04:50 +0300

    must be completed

--- the_second_program.c
+++ the_second_program.c
@@ -1,71 +1,73 @@
 #include <stdio.h>
 #define SIZE 255
 
-int main() {
-    int num = 0;
-    int even = 0;
-    int uneven = 0;
-    int quantity = 0;
-    float quantity_f = 0.0;
-    int positive = 0;
-    int negative = 0;
-    int maximum = -10000;
-    int minimum = 10000;
-    int sum = 0;
-    int index = 0;
-    int mas[SIZE];
+int main()
+{
+    int number;
+    int number_of_elements=0;
+    int positives=0;
+    int negatives=0;
+    int summ=0;
+    int max;
+    int min;
+    float  avarage;
+    float  percentage_of_positives;
+    float  percentage_of_negatives;
+    int array[SIZE];
 
-    while (scanf ("%d ", &num) ==1 /* != EOF*/) {
-        if (num > 10000 || num < -10000){
-            for(index = 0; index < quantity; index++)
-            {
-                printf("%d", mas[index]);
-                if (index != quantity-1)
-                    printf(", ");
-            }
-            printf("\nError: Vstup je mimo interval!\n");
-            return 100;
-        }
-        if(maximum < num)
-            maximum = num;
-        if(minimum > num)
-            minimum = num;
-
-        sum += num;
-        mas[index] = num;
-        index++;
-        quantity++;
-
-        if(num > 0)
-            positive++;
-        if(num < 0)
-            negative++;
-
-        if(num%2 == 0)
-            even++;
-        else
-            uneven++;
-    }
-
-    for(index = 0; index < quantity; index++)
+    for (int i = 0; i < 1;)
     {
-        printf("%d", mas[index]);
-        if (index != quantity-1)
-            printf(", ");
-    }
-    printf("\nPocet cisel: %d\n", quantity);
-    printf("Pocet kladnych: %d\n", positive);
-    printf("Pocet zapornych: %d\n", negative);
-    quantity_f = quantity;
-    printf("Procento kladnych: %.2f\n", (positive/quantity_f)*100);
-    printf("Procento zapornych: %.2f\n", (negative/quantity_f)*100);
-    printf("Pocet sudych: %d\n", even);
-    printf("Pocet lichych: %d\n", uneven);
-    printf("Procento sudych: %.2f\n", (even/quantity_f)*100);
-    printf("Procento lichych: %.2f\n", (uneven/quantity_f)*100);
-    printf("Prumer: %.2f\n", sum/quantity_f);
-    printf("Maximum: %d\n", maximum);
-    printf("Minimum: %d\n", minimum);
+        while ((scanf("%i", &number)) != EOF )
+        {
+            array[number_of_elements] = number;
 
-    return 0;
+            if (number>0) {positives++;}
+            if (number<0) {negatives++;}
+
+	        summ += number;
+            number_of_elements++;
+        }
+
+        i=1;
+    }
+
+	max = array[0];
+	min = array[0];
+
+	for(int i = 0; i < number_of_elements; ++i)
+	{
+		if(array[i] > max)
+		{
+        	max = array[i];
+    	}
+        else if(array[i] < min)
+		{
+			min = array[i];
+		}
+	}
+
+	if(number_of_elements != 0)
+	{
+		avarage = summ/(float)number_of_elements;
+		percentage_of_positives = (positives) / (number_of_elements/100.0);
+		percentage_of_negatives = (negatives) / (number_of_elements/100.0);
+	}
+    else
+	{
+		avarage = 0;
+		percentage_of_positives = 0;
+		percentage_of_negatives=0;
+	}
+
+    /*Final output */
+    printf("Number of elements:\t %i \nPositive numbers: \t %i \nNegative numbers: \t %i \n",
+    number_of_elements, positives, negatives);
+
+    printf("Percentge of positives: \t%.2f%%\n", percentage_of_positives);
+
+    printf("Percentge of negatives: \t%.2f%%\n", percentage_of_negatives);
+
+    printf("Average:\t%.2f\n", avarage);
+
+    printf("Minimum:\t%i\nMaximum:\t%i\n",min, max);
 }
