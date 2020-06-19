4e7fde26e4ffc9118168ddf54a07addf4fdd00ee
AndreiShchepa <andrewbandit228@gmail.com>
2020-06-19 12:30:39 +0300

    complete program

--- the_third_program.c
+++ the_third_program.c
@@ -1,167 +1,177 @@
 #include <stdio.h>
-#define SIZE 3
-int main(void) {
-    int sirka = 0;
-    int vyska = 0;
-    int plot = 0;
-    int bottom_plot = 0;
-    int i = 0;
-    int ii = 0;
-    int iii = 0;
-    int j = 0;
-    int jj = 0;
-    int p1 = 0;
-    int plot_sv = 0;
-    int plot_true = 0;
-    int da = 0;
+#include <time.h>
 
-    da = scanf("%d%d", &sirka, &vyska);
-    if  (da != 2) {
-        fprintf(stderr,"Error: Chybny vstup!\n");
+int main() {
+    clock_t start, end;
+    double cpu_time_used;
+
+    int height;
+    int width;
+    int fence_size;
+
+    char error_msg_100[] = "Error: Chybny vstup!\n";
+    char error_msg_101[] = "Error: Vstup mimo interval!\n";
+    char error_msg_102[] = "Error: Sirka neni sude cislo!\n";
+    char error_msg_103[] = "Error: Neplatna velikost plotu!\n";
+
+    int scan_for_input;
+
+    printf("Enter odd width between 3 and 69.\n");
+    printf("If height is the same as width enter fence size.\n");
+    printf("Fence size mustnt be greater then height.\n");
+
+    scan_for_input = scanf("%d%d", &width, &height);
+
+    if  (scan_for_input !=2)
+    {
+        fprintf(stderr, "%s\n", error_msg_100);
         return 100;
     }
 
-    if (sirka < 3 || sirka > 69 || vyska < 3 || vyska > 69) {
-        fprintf(stderr,"Error: Vstup mimo interval!\n");
-        return 101;
+    if (width == height)
+    {
+        if (scanf("%d", &fence_size) == 0)
+        {
+            fprintf(stderr,"%s", error_msg_100);
+            return 100;
+        }
+        if (fence_size <= 0 || fence_size >= height)
+        {
+            fprintf(stderr, "%s", error_msg_103);
+            return 103;
+        }
+    }else fence_size=0;
 
+    if (width<3 || width>69 || height<3 || height>69)
+    {
+        fprintf(stderr, "%s\n", error_msg_101);
+        return 101;
     }
-    if (sirka%2 == 0) {
-        fprintf(stderr, "Error: Sirka neni liche cislo!\n");
+
+    if (width%2 == 0)
+    {
+        fprintf(stderr, "%s\n", error_msg_102);
         return 102;
     }
 
-    if(sirka == vyska){
-        if (scanf("%d", &plot) == 0){
-            fprintf(stderr,"Error: Chybny vstup!\n");
-            return 100;
-        }
-        if (plot <= 0 || plot >= vyska) {
-            fprintf(stderr, "Error: Neplatna velikost plotu!\n");
-            return 103;
-        }
-    }
-
-    for (i = 0; i < sirka/2; i++) {
-        printf(" ");
-    }
-    printf("X\n");
-
-    for (i = 1, j = sirka/2 - 1, jj = 1; i < sirka/2; i++, j--, jj=jj+2) {
-        for (ii = 0; ii < j; ii++) {
-            printf(" ");
-        }
-        printf("X");
-
-        for(iii = 0; iii < jj; iii++){
-            printf(" ");
-        }
-        printf("X\n");
-    }
-
-    for (i = 0; i < sirka; i++) {
-        printf("X");
-    }
-    printf("\n");
-
-    for (i = 0; i < vyska-2; i++) {
-        if(plot > 0) {
-            printf("X");
-            if (i % 2 == 0) {
-                for (ii = 1; ii < sirka - 1; ii++) {
-                    if (ii % 2 == 0)
-                        printf("*");
-                    else
-                        printf("o");
-                }
-            } else {
-                for (ii = 1; ii < sirka - 1; ii++) {
-                    if (ii % 2 == 0)
-                        printf("o");
-                    else
-                        printf("*");
-                }
-            }
-            printf("X");
-
-            if (i > vyska - plot - 2) {
-                if (i == vyska - plot - 1) {
-                    plot_sv = plot;
-                    if(plot_sv % 2 == 0) {
-                        for (p1 = 1; p1 <= plot; p1++) {
-                            if (p1 % 2 == 0) {
-                                printf("|");
-                            } else {
-                                if (p1 != plot)
-                                    printf("-");
-                            }
-                        }
-                    } else {
-                        for (p1 = 1; p1 <= plot; p1++) {
-                            if (p1 % 2 == 0) {
-                                printf("-");
-                            } else {
-                                if (p1 != plot)
-                                    printf("|");
-                            }
-                        }
-                        printf("|");
-                    }
-                } else {
-                    for (p1 = 1; p1 <= plot; p1++) {
-                        plot_sv = plot;
-                        if(plot_sv % 2 == 0) {
-                            if (p1 % 2 == 0)
-                                printf("|");
-                            else
-                                printf(" ");
-                        } else {
-                            if (p1 % 2 == 0)
-                                printf(" ");
-                            else
-                                printf("|");
-                        }
-                    }
-                }
-            }
-        } else {
-            printf("X");
-            for(ii = 1; ii < sirka-1; ii++){
+    for (int i=0; i<width/2; i++)
+    {
+        for (int j = 0; j<=width*2; j++)
+        {
+            if(j != (width/2)-i && j != (width/2)+i)
+            {
                 printf(" ");
-            }
-            printf("X");
+
+            } else printf("X");
+
         }
         printf("\n");
     }
 
-    for (i = 0; i < sirka; i++) {
-        printf("X");
-    }
-    if(plot > 0) {
-        plot_sv = plot;
-        if(plot_sv % 2 == 0) {
-            plot_true = plot;
-            for (bottom_plot = 1; bottom_plot <= plot_true; bottom_plot++) {
-                if (bottom_plot % 2 == 0)
-                    printf("|");
-                else {
-                    if (bottom_plot != plot_true)
-                        printf("-");
-                }
-            }
-        } else {
-            for (bottom_plot = 1; bottom_plot <= plot; bottom_plot++) {
-                if (bottom_plot % 2 == 0)
-                    printf("-");
-                else {
-                    if (bottom_plot != plot)
-                        printf("|");
-                }
-            }
-            printf("|");
-        }
-    }
-   printf("\n");
+    start = clock();
 
+    for (int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width+fence_size; j++)
+        {
+            if(j >= width)
+            {
+                if (i>=height-fence_size)
+                {
+                    if (i==height-fence_size || i==height-1)
+                    {
+                        if (fence_size%2==0)
+                        {
+                            if (j%2==0)
+                            {
+                                printf("|");
+                            }else printf("-");
+                        }
+                        else
+                        {
+                            if (j%2==0)
+                            {
+                                printf("-");
+                            }else printf("|");
+                        }
+                    }
+                    else
+                    {
+                        if (fence_size%2==0)
+                        {
+                            if (j%2==0)
+                            {
+                                printf("|");
+                            }else printf(" ");
+                        }
+                        else
+                        {
+                            if (j%2==0)
+                            {
+                                printf(" ");
+                            }else printf("|");
+                        }
+                    }
+                }
+            }
+            else
+            {
+              if (height==width)
+                {
+                    if (i==0 || i==width-1)
+                    {
+                        printf("X");
+                    }
+                    else
+                    {
+                        if (j==0 || j==height-1)
+                        {
+                            printf("X");
+                        }
+                        else
+                        {
+                            if (j%2==0 && j>0)
+                            {
+                                if (i%2==0 && i >0)
+                                {
+                                    printf("o");
+
+                                } else printf("*");
+                            }
+                            else
+                            {
+                                if (i%2==0 && i>0)
+                                {
+                                    printf("*");
+
+                                } else printf("o");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                  if (i==0 || i==height-1)
+                  {
+                      printf("X");
+                  }
+                  else
+                  {
+                    if (j==0 || j==width-1)
+                    {
+                      printf("X");
+                    }else printf(" ");
+                  }
+                }
+            }
+        }
+        printf("\n");
+    }
+
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\n");
+    printf("Time of operation = %.5f\n",cpu_time_used);
     return 0;
 }
