e65ded9d6cfbde7953c49edf3e3c83ceb95cf90c
AndreiShchepa <andrewbandit228@gmail.com>
2020-06-22 23:54:49 +0300

    must be completed

--- the_fifth_program.c
+++ the_fifth_program.c
@@ -2,101 +2,191 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#define SIZE 110
+#define SIZE 3
 
-int find_the_key(char* crypted, char* encrypted_w_error)
-{
-    int key;
-    char ch1;
-    char ch2;
-    int array[SIZE];
+/////////////////////////////////////////////
 
-    for (int i = 0; crypted[i] != '\0'; ++i)
-    {
-        ch1 = encrypted_w_error[i];// забираем i-тый char
-        //посмотеть , на сколько передвинут i = тый char
-        ch2 = crypted[i];
-        array[i]=ch2-ch1; // ищем повторяшки в этом массиве! 12 24 12 47 38 12 45
-    }
+int return_size(char* array, char* deleting_array);
+int find_key(char* crypted, char* encrypted_w_error, int loop);
+void function_encrypt(char* crypted, char* encrypted_w_error, int loop, int number);
+char * memory(char *str, int capacity);
+void delete_arrays(char* array1, char* array2);
 
-    int i = k = 0;
-    for(k; array[k] != '\0'; k++) // проходим всеми элементами
-    {
-        i = k+1;
-        do// для k того элемента смотрим все и от 1 до последнего
-        {
-            // 1 2 1 4 4 1
-            if (array[k] == array[i])
-            {
-                printf("%i",array[i]);
-            }
+/////////////////////////////////////////////
 
-            ++i;
-        }
-        while(array[k] != '\0');
-    }
-
-    return key;
-}
-
-void function_encrypt(char* string, int key) // takes an array of chars, key is cipher movement
-{
-    char value;
-    char good_letter;
-    char ch;
-
-    if (key>26){ key %= 26; }else if (key<0) { key += 26; }
-
-    for (int i = 0; string[i] != '\0'; ++i) // проходим каждый символ строки
-    {
-		ch = string[i]; // в i-той итерации ch является i-тым символом
-
-		if (ch >= 'a' && ch <= 'z') // если символ является символом маленького алфавита
-        {
-			ch += key; // подвигаем символ на длину key
-
-
-            /* если мы подвинули так сильно, что вышли за алфавит, то ch -= 26;
-             z - 122 элемент. ch например 123, а 123 это а, но а это 97. 123 - 26 = 97, поэтому 26*/
-			if (ch > 'z') ch -= 26;
-
-            string[i] = ch;
-		}
-
-		else if (ch >= 'A' && ch <= 'Z')
-        {
-			ch += key;
-
-
-			if (ch > 'Z') ch -= 26;
-
-			string[i] = ch;
-		}
-        printf("%c",string[i] );
-	}
-}
-
-void print_string(char* string)
-{
-    for (int i = 0; string[i] != '\0'; ++i)
-    {
-        printf("%c", string[i]);
-    }
-}
+// если ввести frtg-gd то дичь, нужно фиксить, а так норм вроде
 
 int main()
 {
-    char crypt[SIZE];   // chagne to dynamic array
-    char encrypt[SIZE]; // chagne to dynamic array
+    char *crypt = NULL;
+    char *encrypt = NULL;
+    crypt = memory(crypt, SIZE);
+    encrypt = memory(encrypt, SIZE);
+    int length = 0;
 
-    fgets(crypt, sizeof(crypt), stdin);
+    printf("Enter crypted message:                 ");
+    int chars_in_crypt = return_size(crypt, encrypt);
 
-    printf("\n");
+    printf("Enter encrypted message with mistakes: ");
+    int chars_in_encrypt = return_size(encrypt, crypt);
 
-    //print_string(crypt);
-    printf("\n");
-    find_the_key(crypt, encrypt);
-    function_encrypt(crypt, find_the_key);
+    if (chars_in_crypt != chars_in_encrypt)
+    {
+        delete_arrays(crypt, encrypt);
+        fprintf(stderr, "Error: Chybna delka vstupu!\n");
+        return 101;
+    }
+    else
+        length = chars_in_crypt;
 
+    int our_key = find_key(crypt, encrypt, length);
+
+    function_encrypt(crypt, encrypt, length, our_key);
     return 0;
 }
+
+/////////////////////////////////////////////
+
+int return_size(char* working_array, char* deleting_array)
+{
+    int i = 0;
+    int numbers_in_string = SIZE;
+    int chars_in_array = 0;
+
+    while ((working_array[i] = getchar()) != '\n')
+    {
+        if (isalpha(working_array[i]) == 0)
+        {
+            delete_arrays(working_array, deleting_array);
+            fprintf(stderr,"Error: Chybny vstup!\n");
+            return 100;
+        }
+
+        chars_in_array++;
+
+        if (++i == numbers_in_string)
+        {
+            numbers_in_string = numbers_in_string + SIZE;
+            working_array = memory(working_array, numbers_in_string);
+        }
+    }
+    return chars_in_array;
+}
+
+/////////////////////////////////////////////
+
+int find_key(char* crypted, char* encrypted_w_error, int loop)
+{
+    int lalala1 = SIZE + loop;
+    int arr_w_nums[lalala1];
+    int i, k;
+
+    int counter_of_keys = 0;
+
+    for (i = 0; i < lalala1-1; i++)
+    {
+        int shift;
+
+        if ((crypted[i] <=(int)'Z' && crypted[i] >=(int)'A') && (encrypted_w_error[i] > (int)'a' && encrypted_w_error[i] < (int)'z'))
+        {
+            shift = encrypted_w_error[i] - crypted[i] - 6;
+        }else if ((crypted[i] <=(int)'z' && crypted[i] >=(int)'a') && (encrypted_w_error[i] >= (int)'A' && encrypted_w_error[i]<= (int)'Z'))
+        {
+            shift = encrypted_w_error[i] - crypted[i] + 6; // если какая хуйня, то поменять на -
+        }else
+            shift = encrypted_w_error[i] - crypted[i];
+
+        if(shift < 0  ) shift+=52;
+        if(shift > 52 ) shift-=52;
+
+        arr_w_nums[i] = shift;
+    }
+    printf("\n" );
+
+    // тут смотрим повторяшки, чтобы найти нужное передвижение в массиве. Надо создать массив array, в котором будут отмечены передвижения
+   int number = arr_w_nums[0];
+   int max_count = 1;
+   for (i=0; i < lalala1-1; i++) // для каюдого элемента массива. es
+   {
+       int count = 1;// обнуляем как сраку путина
+
+       for (k = i+1; k<lalala1; k++)
+           if (arr_w_nums[i] == arr_w_nums[k]) ++count;
+
+       if (count > max_count)
+       {
+           ++counter_of_keys;
+           max_count = count;
+           number = arr_w_nums[i];
+       }
+   }
+   return number;
+}
+
+/////////////////////////////////////////////
+
+void function_encrypt(char* crypted, char* encrypted_w_error, int loop, int number)
+{
+    int newchar;
+    int lalala1 = SIZE + loop;
+    int i;
+    int arr_w_nums[lalala1];
+
+    for (i = 0; i<lalala1-1; i++)
+    {
+
+        if (arr_w_nums[i]!=number) // если -6 != 10
+        {
+            newchar = (int)crypted[i] + number;
+
+            if (crypted[i] >= 'A' && crypted[i] <= 'Z')
+            {
+                if (newchar <= (int)'a' && newchar >= (int)'Z')
+                    newchar = (newchar - (int)'Z') + (int)'a' - 1;
+
+                if (newchar <= (int)'A')
+                    newchar = (int)'z' - (int)'A' + newchar;
+
+            } else if (crypted[i] >= (int)'a' && crypted[i] <= (int)'z')
+            {
+                if( newchar >= (int)'z' )
+                    newchar = ((int)'A' + newchar) - ((int)'z' + 1);
+
+                else if (newchar <= (int)'a')
+                    newchar = (int)'Z' + (int)'a' - newchar;
+            }
+            encrypted_w_error[i] = newchar;
+       }
+    }
+
+    for (i = -1; i<lalala1; i++)
+    {
+        printf("%c", encrypted_w_error[i]);
+    }
+
+    printf("\n" );
+}
+
+/////////////////////////////////////////////
+
+char * memory(char *increase_message, int capacity)
+{
+    if ((increase_message = (char *)realloc(increase_message, capacity * sizeof(char))) == NULL)
+    {
+        printf("Error!\n");
+        exit(EXIT_FAILURE);
+    }
+    return increase_message;
+}
+
+/////////////////////////////////////////////
+
+void delete_arrays(char* array1, char* array2)
+{
+    free(array1);
+    free(array2);
+    array1 = NULL;
+    array2 = NULL;
+}
+
+/////////////////////////////////////////////
