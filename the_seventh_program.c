5f690a1fe80dbf48b3a56349b356c7bd887ebb30
AndreiShchepa <andrewbandit228@gmail.com>
2020-06-25 19:27:36 +0300

    complete program

--- the_seventh_program.c
+++ the_seventh_program.c
@@ -1,124 +1,175 @@
-//HW07 grep! Hi alive!
 #include <stdio.h>
+#include <stdlib.h>
+/*
+ * Calls function find_prefix, which creates an array with prefixes once
+ * reads file line by line, for each line calls function find_pattern, which finds out, if there is a pattern
+*/
+int read_file(FILE* filename, char* pattern);
+/*
+ * returns an array with matched prefixes
+ *
+*/
+int* prefix_function(char* pattern, int pattern_size);
+/*
+ * Using output from prefix_function finds matching
+ * It there is a matching, returns 0, otherwise -1
+ *
+ */
+int find_pattern (char* string, char* pattern, int* prefix_arr, int pattern_size); // finds pattern and returns 0 if pattern has been found
 
-/* The main program */
-int main(int argc, char *argv[])
+int main( int argc, char* argv[])
+/*argc - arguments counter, argv - argument value.
+ *Получив параметры интероретатор получает параметры, отделенные пробелом
+ *
+ */
 {
-    FILE *fp;
+    char* error_100 = "Error, wrong number of arguments:\n"
+                      "To use this program you have to enter two arguments:\n\tPATTERN which is a string"
+                      " you are looking for \n\tFILEPATH: path to a file where you are looking for a string";
+    char* error_102 = "Error: you've entered a wrong path";
 
-    char pattern[20];
-    int count_pattern = 0;
+    char* error_101 = "Error: sorry, not supported yet";
+    char* error_103 = "Error: error when reading the file occured";
+    char* error_104 = "Error: error when closing the file occured";
 
-    switch (argc){
-        case 2:
-            //[PATTERN]
-            for (count_pattern = 0; argv[1][count_pattern] != '\0'; count_pattern++){
-                pattern[count_pattern] = argv[1][count_pattern];
-            }
-            //[FILE]
-            fp = stdin;
-            break;
+    FILE *working_file;
+
+    switch (argc)
+    {
         case 3:
-            // Ð•NÐ»Ð¸ argv[1][0] == '-', Ð·Ð?Ð°N‡Ð¸N‚ ÐzÐµN€Ð?N‹Ða ÐzÐ°N€Ð°ÐLÐµN‚N€ NTN‚Ðl [OPTIONS]
-            if(argv[1][0] == '-') {
-                //[OPTIONS]
-                //options = argv[1][1];
-
-                //[PATTERN]
-                for (count_pattern = 0; argv[2][count_pattern] != '\0'; count_pattern++){
-                    pattern[count_pattern] = argv[2][count_pattern];
+            /*
+            * открывем файл - 1
+            * выписываем содерюимое рядов, номера рядов, где есть наш паттерн
+             *
+            */
+            if ((working_file=fopen(argv[2], "r")) != NULL) // argv[2] is the FILEPATH - 1
+            {
+                char* pattern = argv[1];
+                switch (read_file(working_file, pattern))
+                {
+                    case 0:
+                        printf("Program completed successfully\n");
+                        return 0;
+                    case 103:
+                        fprintf(stderr, "%s\n",error_103);
+                        return 103;
                 }
-
-                //[FILE]
-                fp = stdin;
-            }
-            else{
-                //[PATTERN]
-                for (count_pattern = 0; argv[1][count_pattern] != '\0'; count_pattern++)
-                    pattern[count_pattern] = argv[1][count_pattern];
-
-                //[FILE]
-                if((fp = fopen(argv[2], "r")) == NULL){
-                    // exit(1);
+                if ( fclose (working_file) == EOF)
+                {
+                    fprintf(stderr, "%s\n",error_104);
+                    return 104;
                 }
-            }
-            break;
+            }else {fprintf(stderr, "%s\n",error_102 ); return 102;}
+
+
         case 4:
-            //[OPTIONS]
-            //options = argv[1][1];
+            fprintf(stderr, "%s\n", error_101);
+            return 101;
 
-            //[PATTERN]
-            for (count_pattern = 0; argv[2][count_pattern] != '\0'; count_pattern++){
-                pattern[count_pattern] = argv[2][count_pattern];
-            }
+        case 5:
+            fprintf(stderr, "%s\n", error_101);
+            return 101;
 
-            //[FILE]
-            if((fp = fopen(argv[2], "r")) == NULL){
-                // exit(1);
-            }
-            break;
+        case 6:
+            fprintf(stderr, "%s\n", error_101);
+            return 101;
+
         default:
-            fp = stdin;
-            break;
+            fprintf(stderr, "%s\n", error_100);
+            return 100;
     }
 
+}
 
-    char *estr = NULL;
-    char buf_string[512];
-    //char options;
-    int found_number = 0;
-    int count_string = 0;
-    int number_string = 0;
+int read_file(FILE* filename, char* pattern)
+{
+    static const int SIZE = 1000;
+    char *str = NULL;
+    char buf_string[SIZE];
 
+    int string_number = 0;
+
+    static int pattern_size = 0;
+    for (int g = 0; pattern[g] != '\0';g++)
+    {
+        pattern_size++;
+    }
+
+    int* d =  prefix_function(pattern, pattern_size);
+    //returns array named d with prefixes matching
+    // ----------------------------------------------------------------
+    // Переменная, в которую поочередно будут помещаться считываемые строки
+    //Указатель, в который будет помещен адрес массива, в который считана
+    // строка, или NULL если достигнут коней файла или произошла ошибка
+
+    //Чтение (построчно) данных из файла в бесконечном цикле
     while (1)
     {
-        // Ð§N‚ÐµÐ?Ð¸Ðµ ÐlÐ´Ð?ÐlÐa NN‚N€ÐlÐsÐ¸  Ð¸Ð· N„Ð°ÐaÐ»Ð°
-        estr = fgets (buf_string,sizeof(buf_string),fp);
-
-        // Ð”Ð»Ð¸Ð?Ð° NN‚N€ÐlÐsÐ¸
-        count_string = 0;
-        while(buf_string[count_string] != '\0')
-            count_string++;
-
-        // ÐšÐlÐ»Ð¸N‡ÐµNN‚Ð?Ðl NN‚N€ÐlÐs
-        number_string++;
-
-
-        if (estr == NULL)
+        string_number++;
+        // Чтение одной строки  из файла
+        str = fgets(buf_string, SIZE, filename);
+        if (str == NULL)
         {
-
-            if ( feof (fp) != 0)
-            {
-
-                break;
-            }
+            // Проверяем, что именно произошло: кончился файл
+            // или это ошибка чтения
+            if ( feof(filename) != 0)
+                return 0; // normal return case
             else
-            {
-
-                break;
-            }
+                return 103;// undefined error. Must not be reached, but...
         }
 
-        int i = 0, ii = 0;
-        for(; i < count_string; i++) {
-            if(buf_string[i] == pattern[ii]){
-                ii++;
-                if(count_pattern == ii){
-                    printf("%s", buf_string);
-                    found_number++;
-                    break;
-                }
-            }
-            else{
-                ii = 0;
-            }
+        if (find_pattern(buf_string, pattern, d, pattern_size) == 0) // return 0, если pattern в строке имеется
+            printf("%d: %s\n", string_number, buf_string);
+
+        //Проверка на конец файла или ошибку чтения
+    }
+}
+
+int* prefix_function (char* pattern, int pattern_size)
+{
+    /* s - строка, в которой мы ищем паттерн, то есть n-тая строка нашего файла
+     * p - паттерн, то есть как раз та подстрока, которую мы ищем в нашем файле
+    */
+    int i, j;
+
+    //int d[pattern_size] = {0};
+    int *d = (int*) malloc( pattern_size);
+    // Вычисление префикс-функции// префикс фция возвр макс значение совпадающих префикса и суфф
+    d[0] = 0;// значения префикс фции записываем в массив d
+    for(i = 1, j = 0; i < pattern_size; i++)// i, j указыважт на символы, которые мы рассматриваем и сравниваем
+    {
+        while(j > 0 && pattern[j] != pattern[i]) // esli j = 0, to d[i] = 0, i++. Inache(j!=0) j = d[j-i].Esli pattern[j] = pattern[i], d[i] = j+1; i++, j++///
+            //то естъ если у нас сначала гуляет i, a j стоит на месте. Если же pattern[i] = pattern[j], то мы двигаем j и i, чтобы найти самое длинное совпадение
+            // длину самого длиннного спвпадения вписытваем в d[i]
+            j = d[j-1];
+        if(pattern[j] == pattern[i])
+            j++;
+        d[i] = j;
+    }
+
+    return d;
+}
+
+int find_pattern(char* str, char* pattern, int* prefix_arr, const int pattern_size)
+{
+    int i, j;
+    int string_len = 0;
+    for(i =0; str[i] != '\0';i++)
+    {
+        string_len ++;
+    }
+
+    for(i = 0, j = 0; i < string_len; i++)
+    {
+        while(j > 0 && (pattern[j] != str[i]))
+            j = prefix_arr[j - 1];
+        if(pattern[j] == str[i])
+            j++;
+        if(j == pattern_size)
+        {
+            return 0;
         }
     }
 
-    fclose(fp);
-
-    if(found_number == 0)
-        return 1;
-
-    return 0;
-
+    return 1;
+}
